36e1ce8859f0c126a1d9d13b687e43ca2fa0098a
DeborahHier <38954263+DeborahHier@users.noreply.github.com>
2018-12-31 19:27:04 -0500

    Add files via upload

--- cgi-bin/recipe.c
+++ cgi-bin/recipe.c
@@ -6,26 +6,26 @@ int main(void) {
     char arg1[MAXLINE], arg2[MAXLINE], arg3[MAXLINE], content[MAXLINE];
             // ----- will the args come in as "cookies" or "q=cookies"?  -----
             //       (just parse if necessary)
-
     char buffer[MAXLINE];
     char buffer2[MAXLINE];
 
     /* Extract the arguments */
     if ((buf = getenv("QUERY_STRING")) != NULL) {
+      //
       p = strchr(buf, '&');
     	*p = '\0';
       strcpy(arg1, buf);
       strcpy(arg2, p+1);
       strcpy(arg2, p+2);
-      char a1 = atoi(arg1);
       // ----- add error handling for if there is only 1 arg -----
       // (i.e. if they just search for a recipe or 1 ingredient)
       // would they be null if they dont exist??
+
       if (arg2 != NULL) {
-        char a2 = atoi(arg2);
+
       }
       if (arg3 != NULL) {
-        char a3 = atoi(arg3);
+
       }
     }
 
@@ -57,17 +57,6 @@ int main(void) {
     Rio_readlineb(&rio, buffer2, MAXLINE);    // get response
     sprintf(content, "%s%s\n", content, buffer2);
     Close(clientfd);
-
-
-    /*  echoclient.c:
-          after establishing a connection with the server, the client enters a loop
-          that repeatedly
-            1) reads a text line from standard input (stdin),             while (Fgets(buf, MAXLINE, stdin) != NULL) {
-            2) sends the text line to the server,                           Rio_writen(clientfd, buf, strlen(buf));
-            3) reads the echo line from the server,                         Rio_readlineb(&rio, buf, MAXLINE);
-            4) and prints the result to standard output (stdout)            Fputs(buf, stdout);
-                                                                          }
-    */
 
     /* Make the response body */
     sprintf(content, "Results for %s: ", arg1);
